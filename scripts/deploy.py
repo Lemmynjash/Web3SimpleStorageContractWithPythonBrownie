5a2e9aee8e528d8cd40e8a6833546900bc23b2b9
Lemmynjash <lemmynjariakiarie@gmail.com>
2022-01-18 17:31:43 +0300

    use of testnet

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,6 +1,23 @@
+from brownie import accounts, Storage,network
+
 
 def deploy_simple_storage():
-    pass
+    #account = accounts[0]
+    account=getaccounts()
+    simple_storage=Storage.deploy({"from": account})
+    store_value=simple_storage.retrieve()
+    print(store_value)
+    transaction_store_value=simple_storage.store(15,{"from": account})
+    transaction_store_value.wait(1)
+    updated_store_value=simple_storage.retrieve()
+    print(updated_store_value)
 
+def getaccounts():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.load("lemmy-account")
+    
+    
 def main():
-    print("hello world")
+    deploy_simple_storage()
